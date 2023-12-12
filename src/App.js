223828fd2aa527df047ce73bc01c6f1084851d7b
noe <noe.jaraq@gmail.com>
2023-12-12 16:44:31 -0500

    new gui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  // return a title for the app as variable
+  const title = 'Welcome to the Road to learn React';
+  // show the title in the browser
+  return (
+    <div>
+      <h1>{title}</h1>
+      // add a text box to enter an url or prompt
+      <label htmlFor="search">URL or prompt: </label>
+      // add a button to submit the url or prompt
+      <input id="search" type="text" />
+      <button type="submit">Search</button>
+      
+    </div>
+  );
 }
 
 export default App;
